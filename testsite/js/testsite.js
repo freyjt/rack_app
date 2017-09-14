4116da50caeaec95d3e300d26b41009bd01e9093
Josh Frey <freyjt@gmail.com>
2017-09-13 20:46:38 -0400

    Cleanup

--- testsite/js/testsite.js
+++ testsite/js/testsite.js
@@ -17,33 +17,20 @@ function WSClickHandler(click_e, caller) {
     caller.style.backgroundColor = ws_e.data
   }
   this.ws.onclose = function(ws_e) {
+    console.log("Lost Websocket Connection")
     destroy_(this)
   }
 }
 
 function main() {
   var click = document.getElementById('click')
-//  var ws = get_connection()
   var ws = null
 
   click.onclick = function(e) {
     if(ws === null) ws = new WSClickHandler(e, this)
     else(console.log("Websocket already active"))
     ws.send("Here's a message " + this.style.backgroundColor);
-//    console.log("Click Registered " + bg_col);
-//    if(bg_col === 'green') bg_col = 'blue';
-//    else bg_col = 'green';
-
   }
-//   ws.onmessage = function(e) {
-//     console.log(e.data)
-//     click.style.backgroundColor = e.data
-//   }
-// 
-//   //does not reset the connection
-//   ws.onclose  = function(e) {
-//     ws = get_connection()
-//   }
 }
 
 
