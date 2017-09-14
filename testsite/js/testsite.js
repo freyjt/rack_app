aea8f732f7cf11ea733d6961e7e46897f223fc9b
Josh Frey <freyjt@gmail.com>
2017-09-13 21:19:40 -0400

    Attempting to add a wait for ready to websocket connection. I don't think I like it

--- testsite/js/testsite.js
+++ testsite/js/testsite.js
@@ -1,5 +1,14 @@
 function get_connection() {
-  return new WebSocket("ws://localhost:8282")
+  return new WebSocket("ws://localhost:8888")
+  
+}
+
+function wait_for_readystate(ws) {
+  console.log("Checking Readystate: " + ws.get_ready_state())
+  setTimeout( function() {
+    if(ws.get_ready_state() === 1) return;
+    else wait_for_readystate(ws);
+  }, 5)
 }
 
 function destroy_(object) {
@@ -7,11 +16,27 @@ function destroy_(object) {
 }
 
 function WSClickHandler(click_e, caller) {
-  this.ws = get_connection();
-  console.log(this.ws);
+  // Websocket may not be ready by the time we call this
   this.send = function(data) {
     this.ws.send(data)
   }
+  this.get_ready_state = function() { return this.ws.readyState }
+  // This only works if we have a callback
+  this.wait_for_readystate = function() {
+    console.log("Checking Readystate: " + this.ws.readyState)
+    setTimeout( function() {
+      if(this.ws.readyState === 1) {
+        console.log("Websocket ready")
+        return;
+      }
+      else this.wait_for_readystate();
+    }, 5)
+  }
+  //constructor
+  this.ws = get_connection();
+  this.wait_for_readystate();
+
+  console.log(this.ws);
   this.ws.onmessage = function(ws_e) {
     console.log(ws_e.data);
     caller.style.backgroundColor = ws_e.data
@@ -22,6 +47,7 @@ function WSClickHandler(click_e, caller) {
   }
 }
 
+
 function main() {
   var click = document.getElementById('click')
   var ws = null
@@ -29,6 +55,9 @@ function main() {
   click.onclick = function(e) {
     if(ws === null) ws = new WSClickHandler(e, this)
     else(console.log("Websocket already active"))
+    console.log("Ready state " + ws.get_ready_state())
+    wait_for_readystate(ws);
+    console.log("After Ready State: " + ws.get_ready_state())
     ws.send("Here's a message " + this.style.backgroundColor);
   }
 }
