ec5f2d6907d05b217cb0b1d7720037d6407cb8a4
Josh Frey <freyjt@gmail.com>
2017-09-15 13:03:59 -0400

    adding some random dots for drawing

--- testsite/js/canvas.js
+++ testsite/js/canvas.js
@@ -1,14 +1,30 @@
-// Constructor
-function CanvasApp() {
 
+
+function Dot(loc, rad) {
+  
 
 }
 
 
+// Constructor
+function CanvasApp(div_id) {
+  can = document.getElementById(div_id);
+  var h = parseInt(can.style.height);
+  var w = parseInt(can.style.width);
+  var d_l = []
+  for(i = 0; i < 10; i++) { 
+    var rad = Math.floor(Math.random()*10) + 5
+    var pos = { x: Math.random() * h, y: Math.random() * w }
+    d_l.push(new Dot(pos, rad))
+  }
+  can.style.border = "2px solid blue"
+  
+
+}
 
 
 function canvas_main() {
-
+  new CanvasApp("canvas")
 
 
 }
