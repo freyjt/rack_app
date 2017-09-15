de810a806ca91e040361f1b61a88f20af7de0160
Josh Frey <freyjt@gmail.com>
2017-09-15 13:07:41 -0400

    Checkpoint. Moving toward drawing function in dot' :

--- testsite/js/canvas.js
+++ testsite/js/canvas.js
@@ -1,10 +1,15 @@
 
 
-function Dot(loc, rad) {
-  
+function Dot(loc, rad) {  
+  this.pos = loc
+  this.rad = rad
 
 }
+Dot.prototype.draw = function(ctx) {
+  ctx.strokeStyle = "#000000";
+  ctx.stroke();
 
+}
 
 // Constructor
 function CanvasApp(div_id) {
@@ -13,12 +18,12 @@ function CanvasApp(div_id) {
   var w = parseInt(can.style.width);
   var d_l = []
   for(i = 0; i < 10; i++) { 
-    var rad = Math.floor(Math.random()*10) + 5
-    var pos = { x: Math.random() * h, y: Math.random() * w }
-    d_l.push(new Dot(pos, rad))
+    var rad = Math.floor(Math.random()*10) + 5;
+    var pos = { x: Math.random() * h, y: Math.random() * w };
+    d_l.push(new Dot(pos, rad));
   }
-  can.style.border = "2px solid blue"
-  
+  can.style.border = "2px solid blue";
+  var ctx = can.getContext('2d');
 
 }
 
