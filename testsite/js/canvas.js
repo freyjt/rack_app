7ce4995a4e4859670a2e79716d9be1b8d120b312
Josh Frey <freyjt@gmail.com>
2017-09-16 14:04:27 -0400

    promoting some variables to class

--- testsite/js/canvas.js
+++ testsite/js/canvas.js
@@ -3,7 +3,8 @@
 function Dot(loc, rad, fillStyle) {  
   this.pos = loc
   this.rad = rad
-  if(fillStyle != null { this.fillStyle = fillStyle }
+  if(fillStyle != null) { this.fillStyle = fillStyle }
+  if(typeof(fillStyle) === "function") { this.fillStyle = fillStyle(); }
 
 }
 Dot.prototype.draw = function(ctx) {
@@ -29,7 +30,6 @@ Flower.prototype.resolvePos = function(angle) {
   y_st = this.pos.y
   x_st = this.pos.x
   var rad = this.rad 
-
   y_new = y_st - (rad * Math.sin(angle))
   x_new = x_st + (rad * Math.cos(angle))
   return {x: x_new, y: y_new}
@@ -38,12 +38,12 @@ Flower.prototype.draw = function(ctx) {
   // lets start by just drawing 6 circles
   var ng = 60;
   var circs = []
-  for(var i = 0; i < 6; i++) { circs.push((ng * i)*(Math.PI / 180)) }
+  for(var i = 1; i < 6; i += 2) { circs.push((ng * i)*(Math.PI / 180)) }
+  ng = 120
+  for(var i = 0; i < 3; i++) { circs.push((ng * i)*(Math.PI / 180)) }
   var dots = []
-  for(var i = 0; i < 6; i++) { 
-    new Dot(this.resolvePos(circs[i]), this.rad).draw(ctx)
-  }
-
+  for(var i = 0; i < 6; i++) { new Dot(this.resolvePos(circs[i]), this.rad, "white").draw(ctx) }
+  new Dot(this.pos, this.rad - 1, "yellow").draw(ctx)
 }
 Flower.prototype.toString = function() {
   return "Position x: " + this.pos.x + " Position y: " + this.pos.y  + " Radius: " + this.rad
@@ -52,34 +52,48 @@ Flower.prototype.toString = function() {
 // Constructor
 function CanvasApp(div_id) {
   var can = document.getElementById(div_id);
-  var h = parseInt(can.style.height);
-  var w = parseInt(can.style.width);
-  can.height = h;
-  can.width = w;
+  this.h = parseInt(can.style.height);
+  this.w = parseInt(can.style.width);
+  can.height = this.h;
+  can.width = this.w;
   var d_l = []
+  can.style.border = "2px solid blue";
+  this.ctx = can.getContext('2d');
   for(i = 0; i < 10; i++) { 
     var rad = Math.floor(Math.random()*10) + 5;
-    var pos = this.getRandPos(h, w);
+    var pos = this.getRandPos();
     d_l.push(new Dot(pos, rad));
   }
-  can.style.border = "2px solid blue";
-  var ctx = can.getContext('2d');
+
+  for(i = 0; i < 10000; i++) {
+    var rad = (Math.random() * 10) + 10;
+    var pos = this.getRandPos();
+    d_l.push(shapeSelector(pos, rad));
+  }
   for(var i in d_l) {
-    d_l[i].draw(ctx);
+    d_l[i].draw(this.ctx);
   } 
-  console.log(d_l);
-  flower = new Flower(this.getRandPos(h, w), 15);
-  console.log(flower.toString())
-  flower.draw(ctx)
- 
 }
-CanvasApp.prototype.getRandPos = function(h, w) {
-  return { x: Math.random() * h, y: Math.random() * w };
+CanvasApp.prototype.getRandPos = function() {
+  return { x: Math.random() * this.w, y: Math.random() * this.h };
+}
+CanvasApp.prototype.renderView = function(drawables) {
+  this.ctx.fillStyle = "#ffffff";
+  this.ctx.fillRect(0, 0, this.w, this.h)
+  for(var i in drawables) {
+    drawables[i].draw()
+  }
 }
 
-
-function canvas_main() {
-  new CanvasApp("canvas")
-
-
+function randomColor() {
+  var colors = ["blue", "yellow", "green", "orange", "red"]
+  return colors[Math.floor(Math.random() * colors.length)]
 }
+
+function shapeSelector(pos, rad) {
+  if(Math.random() < 0.7) { return new Dot(pos, rad, randomColor) }
+  else { return new Flower(pos, rad) }
+}
+
+function canvas_main() { new CanvasApp("canvas") }
+
