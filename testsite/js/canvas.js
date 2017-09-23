3ec3a1acab6d39ccc57d0bcb14292014a462a57d
Josh Frey <freyjt@gmail.com>
2017-09-23 18:21:21 -0400

    Removing useless function definition

--- testsite/js/canvas.js
+++ testsite/js/canvas.js
@@ -1,15 +1,3 @@
-
-// x,y vector with math and builder pattern
-function Pos(x, y) {
-  this.x = x;
-  this.y = y;
-}
-Pos.prototype.add = function(pos) {
-  this.x = this.x + pos.x;
-  this.y = this.y + pos.y;
-  return this;
-}
-
 // Constructor
 function CanvasApp(div_id) {
   var interval_ms = 100;
