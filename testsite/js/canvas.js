3aa3283f36bd4a7275718dee7808bc15a0eb7f22
Josh Frey <freyjt@gmail.com>
2017-09-23 16:45:53 -0400

    Trying out a memoization strategy. Seems to be a performance hit

--- testsite/js/canvas.js
+++ testsite/js/canvas.js
@@ -10,42 +10,6 @@ Pos.prototype.add = function(pos) {
   return this;
 }
 
-
-// spinning flower ...that's what update means
-// @TODO better names for properties
-function DFlower(loc, rad, petal_f, stamen_f, spin) {
-  this.dot = new Dot(loc, rad / 4, stamen_f);
-  this.loc = loc
-  this.d_len = rad;
-  this.d_d = rad / 4;
-  this.d_w = rad / 2;
-  this.d__l = (3*this.d_len)/4;
-  this.rot = 45;
-  this.petal_f = petal_f
-  this.spin = spin
-  if(typeof(this.spin) != "number") this.spin = 0;
-  this.total_spin = 0.0;
-  this.radianCache = new IntegerRad()
-  this.formPetals();
-}
-DFlower.prototype.draw = function(ctx) {
-  this.dot.draw(ctx)
-  for(var i in this.petals) {
-    this.petals[i].draw(ctx);
-  }
-}
-DFlower.prototype.update = function( ) {
-  this.total_spin = this.total_spin + this.spin
-  this.formPetals()
-}
-DFlower.prototype.formPetals = function() {
-  this.petals = [];
-  for(var i = 0; i < (360 / this.rot); i++) {
-    this.petals.push(new Diamond(this.loc, this.d_d, this.d_len, this.d_w, this.d__l, this.radianCache.fromD((this.rot * i) + this.total_spin), this.petal_f))
-  } 
-}
-
-
 // Constructor
 function CanvasApp(div_id) {
   var interval_ms = 100;
@@ -73,35 +37,12 @@ function CanvasApp(div_id) {
     this.d_l.push(new Dot(pos, rad, randomColor()));
   }
   caller = this
+  caller.iterateView();
+  caller.renderView();
   intId = setInterval(function() {
     caller.iterateView()
     caller.renderView();
   }, interval_ms);
-
-
-//  this.renderView(this.d_l);
-// Set us up the canvas
-//  can.onclick = function(e) {
-//    this.d_l = []
-//    caller.renderView(this.d_l)
-//    var v_req = { "Purpose": "Request View", "height": caller.h, "width": caller.w };
-//    caller.ws.send(JSON.stringify(v_req));
-//  }
-//  this.ws.onmessage = function(e) {
-//    console.log(e.data)
-//    var objs = JSON.parse(e.data);
-//    // Assumes objs is an array
-//    for(var i in objs) {
-//      var obj = objs[i]
-//      var pos = obj.pos
-//      var rad = obj.rad
-//      var fill = obj.fill
-//      if(obj.type == "Flower") { var shape = new Flower(pos, rad, fill) }
-//      else{ var shape = new Dot(pos, rad, fill) }
-//      this.d_l.push(shape);
-//    }
-//    caller.renderView(this.d_l);
-//  }
 }
 CanvasApp.prototype.getRandPos = function() {
   return new Pos(Math.random() * this.w, Math.random() * this.h);
@@ -119,17 +60,16 @@ CanvasApp.prototype.iterateView = function() {
   }
 }
 
-
 function IntegerRad() {
    this.memo = []
    var p_over = Math.PI / 180
    for(var i = 0; i < 360; i++)
-      memo.push(p_over * i)
+      this.memo.push(p_over * i)
 }
 IntegerRad.prototype.fromD = function(d) { 
-  if(Number.isInteger(d)
+  if(Number.isInteger(d))
     return this.memo[d % 360];
-  else return (Math.PI / 180) * d }
+  else return (Math.PI / 180) * d
 }
 
 function randomColor() {
@@ -149,4 +89,3 @@ function num_bet_mag_and_opp(mag) {
 }
 
 function canvas_main() { new CanvasApp("canvas") }
-
--- testsite/js/diamond.js
+++ testsite/js/diamond.js
@@ -2,49 +2,56 @@
 // We can memo length too, but idk if that makes sense just yet
 // @NOTE This isn't premature. Excuses aside, we were getting some lag on the runs,
 //   ideally this makes it better?
+// Say....dumbass...why are you going with 360.... these functions work on radians....
 function Trig( ) {
-  this.sin = []
-  this.cos = [] 
+  this.sin_memo = []
+  this.cos_memo = [] 
   for(var i = 0; i < 360; i++) {
-    this.sin.push(Math.sin(i));
-    this.cos.push(Math.cos(i));
+    this.sin_memo.push(Math.sin(Trig.radsFromDegs(i)));
+    this.cos_memo.push(Math.cos(Trig.radsFromDegs(i)));
   }
 }
 Trig.prototype.sin = function(d) {
-  if(Number.isInteger(d))
-    return this.sin[d % 360];
+  if(Number.isInteger(d)) {
+    return this.sin_memo[d % 360];
+  }
   else
-    return Math.sin(d);
+    return Math.sin(Trig.radsFromDegs(d));
 }
 Trig.prototype.cos = function(d) {
-  if(Number.isInteger(d))
-    return this.cos[d % 360];
+  if(Number.isInteger(d)) {
+    return this.cos_memo[d % 360];
+  }
   else
-    return Math.sin(d);
+    return Math.cos(Trig.radsFromDegs(d));
 }
-
+Trig.radsFromDegs = function(degree) { return (Math.PI * degree) / 180; }
+Trig.degsFromRads = function(rads) { return (rads * 180) / Math.PI; }
 
 // start_pos is given to be the point one of the diamond, which all other measurements will be taken agianst
 // start_pos and rot need to coincide, @TODO make this not true rot ^ dist implies start
 function Diamond(pos, start_dist, len, wid, len_to_w, rot, fillStyle) {
-  static trig = new Trig();
   var half_wid = wid / 2;
-  var wing_theta = Math.atan(half_wid / len_to_w)
-  var wing_hyp = Math.cos(wing_theta) * len_to_w
+  var wing_theta = Math.round(Trig.degsFromRads(Math.atan(half_wid / len_to_w))); // we should round this so we can take advantage of the memo
+  console.log( " WING THETA: " + wing_theta ) 
+  var wing_hyp = Diamond.trig.cos(wing_theta) * len_to_w
   if(typeof(rot) != "number") { rot = 0 }
-  start_vec = new Pos( (Math.cos(rot) * start_dist), -(Math.sin(rot) * start_dist));
+  rot = Trig.degsFromRads(rot);
+  start_vec = new Pos( (Diamond.trig.cos(rot) * start_dist), -(Diamond.trig.sin(rot) * start_dist));
+
   this.points = []
-  t_n = 0;                h_n = 0;        this.points.push(pos_closure().add(start_vec).add(pos))
-  t_n = rot - wing_theta; h_n = wing_hyp; this.points.push(pos_closure().add(start_vec).add(pos))
-  t_n = rot;              h_n = len;      this.points.push(pos_closure().add(start_vec).add(pos))
-  t_n = rot + wing_theta; h_n = wing_hyp; this.points.push(pos_closure().add(start_vec).add(pos))
+  t_n = 0;                h_n = 0;        this.points.push(this.pos(t_n, h_n).add(start_vec).add(pos))
+  t_n = rot - wing_theta; h_n = wing_hyp; this.points.push(this.pos(t_n, h_n).add(start_vec).add(pos))
+  t_n = rot;              h_n = len;      this.points.push(this.pos(t_n, h_n).add(start_vec).add(pos))
+  t_n = rot + wing_theta; h_n = wing_hyp; this.points.push(this.pos(t_n, h_n).add(start_vec).add(pos))
 
   if(fillStyle != null) this.fillStyle = fillStyle;
   if(typeof(fillStyle) == "function") this.fillStyle = fillStyle();
-
-  function pos_closure() {
-    return new Pos( This.trig.cos(t_n) * h_n, -(This.trig.sin(t_n) * h_n))
-  }
+}
+// @TODO Trig should just be static forcing it to be static here means we need to re-init for every object that it is useful for
+Diamond.trig = new Trig();
+Diamond.prototype.pos = function(theta, hyp) {
+  return new Pos( Diamond.trig.cos(theta) * hyp, -(Diamond.trig.sin(theta) * hyp));
 }
 Diamond.prototype.draw = function(ctx) {
   ctx.strokeStyle = "#000000";
